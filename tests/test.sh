a64525cd6e56f7809295676ae31fb9dee80fa3c6
Denellyne <gustavobarbosavs@gmail.com>
2025-11-05 20:06:48 +0000

    better tester

--- tests/test.sh
+++ tests/test.sh
@@ -12,12 +12,14 @@ fails=0
 for filename in ./*.adb; do
   tests=$((tests + 1))
   if ../bin/ada $filename >/dev/null 2>&1; then
-    echo $filename Success
+    echo -e "\033[0;32m$filename Success\033[0m"
     successes=$((successes + 1))
   else
-    echo $filename Failed
+    echo -e "\033[0;31m$filename Failed\033[0m"
     fails=$((fails + 1))
   fi
 done
 
-echo "Tests:$tests | Success:$successes | Fail:$fails"
+echo -e -n Tests:$tests
+echo -e -n " |\033[0;32m Success:$successes\033[0m |"
+echo -e "\033[0;31m Fail:$fails\033[0m"
